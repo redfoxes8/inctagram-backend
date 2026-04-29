eab09c4a839a476d3a31364a9378411c25ca677f
Alexander0Yusov <yusovsky2@gmail.com>
2026-04-30 02:34:05 +0300

    IN-83 ci: test deploy

--- apps/main-gateway-service/src/app.module.ts
+++ apps/main-gateway-service/src/app.module.ts
@@ -13,7 +13,6 @@ import { SessionsModule } from './modules/sessions/sessions.module';
 import { GoogleRecaptchaModule } from '@nestlab/google-recaptcha';
 import { CoreConfig } from '../../../libs/common/src/core.config';
 
-
 @Module({
   imports: [
     CommonModule,
