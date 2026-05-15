7bc5d91a0eaedab659e44dfd8a6beaebf948c444
Alexander0Yusov <yusovsky2@gmail.com>
2026-05-16 00:57:33 +0300

    IN-94 refactor(files): introduce application layer and persist pending status

--- apps/micro-files-service/src/modules/files/files.module.ts
+++ apps/micro-files-service/src/modules/files/files.module.ts
@@ -1,11 +1,33 @@
 import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
 
 import { FilesConfigModule } from '../../core/files-config.module';
 import { AwsStorageAdapter } from '../files/infrastructure/aws-storage.adapter';
+import { IStorageAdapter } from './application/interfaces/storage-adapter.interface';
+import { FilesRepository } from './infrastructure/files.repository';
+import { IFilesRepository } from './domain/interfaces/files.repository.interface';
+import { GenerateUploadUrlUseCase } from './application/use-cases/generate-upload-url.use-case';
+import { FilesController } from './api/files.controller';
+import { PrismaService } from '../../core/prisma/prisma.service';
+
+const useCases = [GenerateUploadUrlUseCase];
+const repositories = [
+  {
+    provide: IFilesRepository,
+    useClass: FilesRepository,
+  },
+];
+const adapters = [
+  {
+    provide: IStorageAdapter,
+    useClass: AwsStorageAdapter,
+  },
+];
 
 @Module({
-  imports: [FilesConfigModule],
-  providers: [AwsStorageAdapter],
-  exports: [AwsStorageAdapter],
+  imports: [CqrsModule, FilesConfigModule],
+  controllers: [FilesController],
+  providers: [PrismaService, ...useCases, ...repositories, ...adapters],
+  exports: [IStorageAdapter, IFilesRepository],
 })
 export class FilesModule {}
